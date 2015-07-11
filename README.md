2c8221b1e3ed712af7dc72f1b760122b1b607690
Raniere Silva <raniere@ime.unicamp.br>
2015-07-11 15:44:14 -0300

    Version 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Software Carpentry Shell Split Window
+
+This is a script to split window for the shell lesson.
+
+## License
+
+Under MIT.
+
+## Requirement
+
+-   [tmux](https://tmux.github.io/)
+
+## Use
+
+~~~
+$ swc-shell-split-window.sh
+~~~
---
+++ swc-shell-split-window.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Create terminal for Software Carpentry lesson
+# with the log of the commands at the top.
+
+LOG_FILE=/tmp/log-file
+
+# Create the session to be used
+tmux new-session -d -s swc
+# Split the window
+tmux split-window -v
+# Launch Bash since this hack only works with it
+tmux send-keys -t 1 "bash" enter
+tmux send-keys -t 1 "cd" enter
+# Unset alias
+tmux send-keys -t 1 "unalias grep" enter
+tmux send-keys -t 1 "unalias ls" enter
+tmux send-keys -t 1 "unalias sort" enter
+# Set nice prompt displaying
+# with cyan
+# the command number and
+# the '$'.
+tmux send-keys -t 1 "export PS1=\"\[\033[1;36m\]\! $\[\033[0m\] \"" enter
+# Hack to write the log after every command
+tmux send-keys -t 1 "export PROMPT_COMMAND=\"history 1 >> ${LOG_FILE}\"" enter
+for i in $(seq 5)
+do
+    tmux send-keys -t 1 "clear" enter
+done
+# Resize the log window to show the last five commands
+# Need to use the number of lines desired + 1
+tmux resize-pane -t 0 -y 6
+# Remove old file and create a empty one
+rm -f ${LOG_FILE}
+touch ${LOG_FILE}
+# Starting read the log
+tmux send-keys -t 0 "tail -n 3 -f ${LOG_FILE}" enter
+
+tmux attach-session
